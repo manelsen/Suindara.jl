c96962318d378d31e62608f1a9220e7cd6ee9ab7
manelsen <emanuelmoura@gmail.com>
2026-02-17 21:14:46 -0300

    chore: remove benchmark/ and benchmarks/ directories

--- benchmarks/run_genie.jl
+++
@@ -1,7 +0,0 @@
-using Genie, Genie.Router, Genie.Renderer.Json
-
-route("/") do
-  json(Dict("message" => "Hello World"))
-end
-
-up(8085, async=false)
--- benchmarks/run_oxygen.jl
+++
@@ -1,9 +0,0 @@
-using Oxygen
-using HTTP
-using JSON3
-
-@get "/" function(req::HTTP.Request)
-    return Dict("message" => "Hello World")
-end
-
-serve(port=8086)
